477d28db3c96eb54639cffa6d9678f2df9a2bf39
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-04-10 10:06:36 +0200

    Search functionality

--- src/components/shared/MainHeader.jsx
+++ src/components/shared/MainHeader.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/logo.png";
 
-export default function MainHeader() {
+export default function MainHeader({ searchValue, setSearchValue }) {
+  function changeHandler(event) {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <header>
       <Link to="/" className="logo">
         <img src={logo} alt="Netflix logo" />
       </Link>
+      <div className="search">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Titles"
+          value={searchValue}
+          onChange={changeHandler}
+        ></input>
+      </div>
     </header>
   );
 }
--- src/pages/customer/Home.jsx
+++ src/pages/customer/Home.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 
 import { readDocuments } from "../../scripts/firebase/fireStore";
 import { useItems } from "../../state/ItemsContext";
+import MainHeader from "../../components/shared/MainHeader";
 import MediaCarousel from "../../components/MediaCarousel";
 import HeadingExpand from "../../components/shared/HeadingExpand";
 
 export default function Home() {
   const { items, dispatch } = useItems();
   const [status, setStatus] = useState("loading");
+  const [searchValue, setSearchValue] = useState("");
 
   const path = "titles";
 
@@ -31,32 +33,53 @@ export default function Home() {
   }
 
   const filterItemsByType = (type) => {
-    return items.filter((item) => item.type === type);
+    return items.filter(
+      (item) =>
+        item.type === type &&
+        item.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
   };
 
   const movies = filterItemsByType("movie");
   const series = filterItemsByType("series");
   const documentaries = filterItemsByType("documentary");
 
-  // console.log(series);
-
-  // const media = items.map((item) => (
-  //   <MediaCarousel key={item.id} item={item} />
-  // ));
-
   if (status === "loading") return <p>Loading...</p>;
   if (status === "error") return <p>Error</p>;
 
   return (
     <div id="home">
-      <HeadingExpand label="Movies" />
-      <MediaCarousel items={movies} />
+      <MainHeader searchValue={searchValue} setSearchValue={setSearchValue} />
 
-      <HeadingExpand label="Series" />
-      <MediaCarousel items={series} />
+      <div className="media">
+        {movies.length !== 0 && (
+          <div>
+            <HeadingExpand label="Movies" />
+            <MediaCarousel items={movies} />
+          </div>
+        )}
 
-      <HeadingExpand label="Documentaries" />
-      <MediaCarousel items={documentaries} />
+        {series.length !== 0 && (
+          <div>
+            <HeadingExpand label="Series" />
+            <MediaCarousel items={series} />
+          </div>
+        )}
+
+        {documentaries.length !== 0 && (
+          <div>
+            <HeadingExpand label="Documentaries" />
+            <MediaCarousel items={documentaries} />
+          </div>
+        )}
+      </div>
+
+      {searchValue &&
+        movies.length == 0 &&
+        series.length == 0 &&
+        documentaries.length == 0 && (
+          <p>Your search for "{searchValue}" did not have any matches. </p>
+        )}
     </div>
   );
 }
