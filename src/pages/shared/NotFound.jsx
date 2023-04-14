5ceace561016e03be0e85a20c0bfe2007f3d10c8
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-04-14 22:20:41 +0200

    Page not found

--- src/pages/shared/NotFound.jsx
+++ src/pages/shared/NotFound.jsx
@@ -1,3 +1,23 @@
+import { useNavigate } from "react-router";
+import BasicHeader from "../../components/shared/BasicHeader";
+
 export default function NotFound() {
-  return <div>Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="not-found">
+      <BasicHeader pageName="not-found" />
+
+      <div className="text">
+        <h1>Lost your way?</h1>
+        <p>
+          Sorry, we can't find that page. You'll find loads to explore on the
+          home page.
+        </p>
+        <button className="white-button" onClick={() => navigate("/")}>
+          Netflix Home
+        </button>
+      </div>
+    </div>
+  );
 }
