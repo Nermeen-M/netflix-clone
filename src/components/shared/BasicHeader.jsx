c6a4e8e3c15fd382adaeffb43b2bae4cecbd45c1
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-04-14 21:59:27 +0200

    Auth styles

--- src/components/shared/BasicHeader.jsx
+++ src/components/shared/BasicHeader.jsx
@@ -4,10 +4,13 @@ import logo from "../../assets/images/logo.png";
 
 export default function BasicHeader({ pageName }) {
   return (
-    <header className="basic-header">
+    <header
+      className={pageName !== "login" ? "basic-header light" : "basic-header"}
+    >
       <Link to="/" className="logo">
         <img src={logo} alt="Netflix logo" />
       </Link>
+      {pageName !== "login" && <Link to="/login">Sign In</Link>}
     </header>
   );
 }
